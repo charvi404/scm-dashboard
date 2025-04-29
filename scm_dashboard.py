10ea1634cc8fdd7f0f7296431238e89c97557170
charvi404 <charvi.srivastava2020@gmail.com>
2025-04-29 23:01:42 +0530

    Update scm_dashboard.py

--- scm_dashboard.py
+++ scm_dashboard.py
@@ -635,6 +635,7 @@ elif page == "Predictive Modeling":
                 
                 # Handle nulls in features
                 X = X.fillna(X.mean())
+                y = y.fillna(y.mean())
                 
                 # Train-test split
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
